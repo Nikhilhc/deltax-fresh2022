cd834a9608645d479fdf6d4e13a7f93a112849d3
nikil gowda <nikilhc@gmail.com>
2022-06-02 22:18:00 +0530

    updated

--- Deltax/urls.py
+++ Deltax/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/',include('deltax_spotify.urls'))
+    path('deltax/',include('deltax_spotify.urls'))
 ]
