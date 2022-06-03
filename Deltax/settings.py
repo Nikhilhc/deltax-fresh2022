eadd1a5e66cd1fca4f589a293cc71334d78f4f7e
nikil gowda <nikilhc@gmail.com>
2022-06-03 17:52:02 +0530

    updated

--- Deltax/settings.py
+++ Deltax/settings.py
@@ -128,3 +128,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
